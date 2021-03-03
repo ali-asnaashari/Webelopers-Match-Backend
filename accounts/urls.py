5e86a0813ccdb9d26dae8082ba8168de34b8dcd3
mahmoud <mahmoud.kheyrati.fard@gmail.com>
2021-03-03 23:37:27 +0330

    login, signup, logout added (without style)

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,2 +1,13 @@
+from django.contrib.auth.views import LoginView
+from django.urls import path
+
+from accounts.views import signup, logout_user
 
 app_name = 'accounts'
+
+urlpatterns = [
+    path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('signup/', signup, name='signup'),
+    path('logout/', logout_user, name='logout')
+
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
-# Create your views here.
+from accounts.forms import SignUpForm
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            raw_password2 = form.cleaned_data.get('password2')
+
+            if raw_password != raw_password2:
+                pass
+
+            return redirect('home')
+
+            # return HttpResponse("ok ")
+    else:
+        form = SignUpForm()
+    return render(request, 'accounts/signup.html', {'form': form})
+
+
+def logout_user(request):
+    logout(request)
+    return redirect(reverse('home'))
+
