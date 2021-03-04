09888282de98f90083b1c13b23ff0bde718c45c1
mahmoud <mahmoud.kheyrati.fard@gmail.com>
2021-03-04 11:50:19 +0330

    change login error

--- WebelopersMatchBackend/settings.py
+++ WebelopersMatchBackend/settings.py
@@ -126,17 +126,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL = '/'
 
-
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'lolipopland2014@gmail.com'
 EMAIL_HOST_PASSWORD = '189700100'
-
-DBTEMPLATES_CACHE_BACKEND = 'file://'+os.path.join(os.path.dirname(__file__), 'db/')+'django.cache'
-
-
-
-
