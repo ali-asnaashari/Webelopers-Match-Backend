4020e0ba2e34e9efa2d8b685b6a39501a738d81f
mahmoud <mahmoud.kheyrati.fard@gmail.com>
2021-03-04 08:12:36 +0330

    login id and signup id fixed

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,9 +4,15 @@ from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.', )
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    username = forms.CharField(widget=forms.TextInput(attrs={'id': 'username'}))
+    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.',
+                                 widget=forms.TextInput(attrs={'id': 'first_name'}))
+    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.',
+                                widget=forms.TextInput(attrs={'id': 'last_name'}))
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.',
+                             widget=forms.TextInput(attrs={'id': 'email'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'id': 'password1'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'id': 'password2'}))
 
     class Meta:
         model = User
