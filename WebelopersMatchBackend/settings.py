6bd98c93a26c4156443133d6b7ce31096748cc73
mahmoud <mahmoud.kheyrati.fard@gmail.com>
2021-03-05 09:16:37 +0330

    email change password

--- WebelopersMatchBackend/settings.py
+++ WebelopersMatchBackend/settings.py
@@ -131,4 +131,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'lolipopland2014@gmail.com'
-EMAIL_HOST_PASSWORD = '189700100'
+EMAIL_HOST_PASSWORD = 'mmmaaahmoud'
